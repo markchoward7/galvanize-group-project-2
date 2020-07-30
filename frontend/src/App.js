91b1d1888aace7ff9d5be58cc18bf029e7a7e260
markchoward7 <markchoward7@outlook.com>
2020-07-30 13:49:36 -0500

    restructured react directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,15 @@ import {
 
 import './App.css';
 
-import Home from './Home'
-import NewTasking from './NewTasking'
-import NewUser from './NewUser'
-import Sidebar from './Sidebar'
-import TaskingDetail from './TaskingDetail'
-import UserList from './UserList'
-import UserDetail from './UserDetail'
-import UpdateTasking from './UpdateTasking'
-import UpdateUser from './UpdateUser'
+import Home from './components/Home'
+import NewTasking from './components/NewTasking'
+import NewUser from './components/NewUser'
+import Sidebar from './components/Sidebar'
+import TaskingDetail from './components/TaskingDetail'
+import UserList from './components/UserList'
+import UserDetail from './components/UserDetail'
+import UpdateTasking from './components/UpdateTasking'
+import UpdateUser from './components/UpdateUser'
 
 const axios = require('axios').default
 axios.defaults.headers.post['Content-Type'] = 'application/json'
